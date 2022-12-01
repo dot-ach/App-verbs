5149bf0a35402a1dcd679e57214404f9197ec5d7
Alan <holasoyalan9@gmail.com>
2022-11-30 23:59:16 -0600

    aaaaaa, pude agregar un tiempo verbal por verbo

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-// import { regularVerbs } from "./verbs";
-// import { irregularVerbs } from "./verbs";
 
-// // const tensify = require('tensify');
-// const getTensify = async () => {
-  //   const result = await fetch('tensify');
-  //   const object = await result.json();
-  //   console.log(object);x
-  // }
-  // console.log(tensify('do').past);
-  
-  console.log(regularVerbs[0].infinitive, irregularVerbs)
-  // Creating container and inputs
+// Creating container and inputs
 const container = document.querySelector('.container');
+
 const inputsAdding = () => {
   const inputInfinitive = document.createElement('input');
   inputInfinitive.placeholder = 'Infinitive';
@@ -32,8 +22,55 @@ const inputsAdding = () => {
 
 }
 
+const randomNumber = (max, min) => {
+  const result = Math.floor((Math.random() * (max - min + 1)) + min);
+  return result;
+};
 
-for (let index = 0; index < 4; index++) {
-  inputsAdding();
-  console.log(index);
-}
+
+console.log(randomNumber(6, 1));
+
+regularVerbs.forEach((item, counter) => {
+  // console.log(counter)
+  
+  const tenseNumber = randomNumber(5, 1);
+  
+  const inputInfinitive = document.createElement('input');
+  inputInfinitive.placeholder = 'Infinitive';
+  if(tenseNumber === 1){
+    inputInfinitive.value = item.infinitive;
+  }
+  
+  const inputPast = document.createElement('input');
+  inputPast.placeholder = 'Past';
+  if(tenseNumber === 2){
+    inputPast.value = item.past;
+  }
+  
+  const inputPastParticiple = document.createElement('input');
+  inputPastParticiple.placeholder = 'Past Participle';
+  if(tenseNumber === 3){
+    inputPastParticiple.value = item.pastParticiple;
+  }
+  
+  const inputGeround = document.createElement('input');
+  inputGeround.placeholder = 'Geround';
+  if(tenseNumber === 4){
+    inputGeround.value = item.geround;
+  }
+  
+  const inputSpanish = document.createElement('input');
+  inputSpanish.placeholder = 'Spanish';
+  if(tenseNumber === 5){
+    inputSpanish.value = item.spanish;
+  }
+
+  container.append(inputInfinitive, inputPast, inputPastParticiple, inputGeround, inputSpanish);
+});
+
+// for (let index = 0; index < 4; index++) {
+//   inputsAdding();
+//   console.log(index);
+// }
+
+
--- verbs.js
+++ verbs.js
@@ -1,10 +1,29 @@
-const emptyObject = {infinitive: '', trdPerson: '', past: '', pastParticiple: '',geround: '', spanish: ''}
+const emptyObject = {infinitive: '', trdPerson: '', past: '', pastParticiple: '', geround: '', spanish: ''}
+
 const regularVerbs = [
-  {infinitive: 'open', trdPerson: 'opens', past: 'opened', pastParticiple: 'opened',geround: 'opening', spanish: 'abrir'},
+  {infinitive: 'open', trdPerson: 'opens', past: 'opened', pastParticiple: 'opened', geround: 'opening', spanish: 'abrir'},
+  {infinitive: 'accept', trdPerson: 'accepts', past: 'accepted', pastParticiple: 'accepted', geround: 'accepting', spanish: 'aceptar'},
+  {infinitive: 'love', trdPerson: 'loves', past: 'loved', pastParticiple: 'loved', geround: 'loving', spanish: 'amar'},
+  {infinitive: 'learn', trdPerson: 'learns', past: 'learned', pastParticiple: 'learned', geround: 'learning', spanish: 'aprender'},
+  {infinitive: 'hurry', trdPerson: 'hurries', past: 'hurried', pastParticiple: 'hurried', geround: 'hurrying', spanish: 'apresurarse'},
+  {infinitive: 'dare', trdPerson: 'dares', past: 'dared', pastParticiple: 'dared', geround: 'daring', spanish: 'atreverse'},
+  {infinitive: 'help', trdPerson: 'helps', past: 'helped', pastParticiple: 'helped', geround: 'helping', spanish: 'ayudar'},
+  {infinitive: 'dance', trdPerson: 'dances', past: 'danced', pastParticiple: 'danced', geround: 'dancing', spanish: 'bailar'},
+  {infinitive: 'erase', trdPerson: 'erases', past: 'erased', pastParticiple: 'erased', geround: 'erasing', spanish: 'borrar'},
+  {infinitive: 'change', trdPerson: '', past: 'changes', pastParticiple: 'changed', geround: 'changing', spanish: 'cambiar'},
 ]
 
 const irregularVerbs = [
-  {infinitive: 'sweep', trdPerson: 'sweeps', past: 'swept', pastParticiple: 'swept',geround: 'sweeping', spanish: 'barrer'},
+  {infinitive: 'sweep', trdPerson: 'sweeps', past: 'swept', pastParticiple: 'swept', geround: 'sweeping', spanish: 'barrer'},
+  {infinitive: 'drink', trdPerson: 'drinks', past: 'drank', pastParticiple: 'drunk', geround: 'drinking', spanish: 'beber'},
+  {infinitive: 'ride', trdPerson: 'rides', past: 'rode', pastParticiple: 'ridden', geround: 'riding', spanish: 'cabalgar'},
+  {infinitive: 'fall', trdPerson: 'falls', past: 'fell', pastParticiple: 'fallen', geround: 'falling', spanish: 'caer'},
+  {infinitive: 'sing', trdPerson: 'sings', past: 'sang', pastParticiple: 'sung', geround: 'singing', spanish: 'cantar'},
+  {infinitive: 'set', trdPerson: 'sets', past: 'set', pastParticiple: 'set', geround: 'setting', spanish: 'colocar'},
+  {infinitive: 'eat', trdPerson: 'eats', past: 'ate', pastParticiple: 'eaten', geround: 'eating', spanish: 'comer'},
+  {infinitive: 'buy', trdPerson: 'buys', past: 'bought', pastParticiple: 'bought', geround: 'buying', spanish: 'comprar'},
+  {infinitive: 'meet', trdPerson: 'meets', past: 'met', pastParticiple: 'met', geround: 'meeting', spanish: 'conocer'},
+  {infinitive: 'know', trdPerson: 'knows', past: 'knew', pastParticiple: 'known', geround: 'knowing', spanish: 'saber'},
 ]
 
 // export {regularVerbs, irregularVerbs};
