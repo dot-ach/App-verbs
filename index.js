6ad188a82c9e471a45f4293a459cbe3ed0ab43a4
Alan <holasoyalan9@gmail.com>
2022-12-07 00:39:41 -0600

    Agregué un id a cada imput para después usar ese id para hacer las validaciones de los verbos

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 // Creating container and inputs
 const container = document.querySelector('.container');
 
+const button = document.querySelector('.btn__main');
+
 const inputsAdding = () => {
   const inputInfinitive = document.createElement('input');
   inputInfinitive.placeholder = 'Infinitive';
@@ -37,36 +39,47 @@ regularVerbs.forEach((item, counter) => {
   
   const inputInfinitive = document.createElement('input');
   inputInfinitive.placeholder = 'Infinitive';
+  inputInfinitive.id = item.infinitive;
   if(tenseNumber === 1){
     inputInfinitive.value = item.infinitive;
   }
   
   const inputPast = document.createElement('input');
   inputPast.placeholder = 'Past';
+  inputPast.id = item.past;
   if(tenseNumber === 2){
     inputPast.value = item.past;
   }
   
   const inputPastParticiple = document.createElement('input');
   inputPastParticiple.placeholder = 'Past Participle';
+  inputPastParticiple.id = item.pastParticiple;
   if(tenseNumber === 3){
     inputPastParticiple.value = item.pastParticiple;
   }
   
   const inputGeround = document.createElement('input');
   inputGeround.placeholder = 'Geround';
+  inputGeround.id = item.geround;
   if(tenseNumber === 4){
     inputGeround.value = item.geround;
   }
   
   const inputSpanish = document.createElement('input');
   inputSpanish.placeholder = 'Spanish';
+  inputSpanish.id = item.spanish;
   if(tenseNumber === 5){
     inputSpanish.value = item.spanish;
   }
 
   container.append(inputInfinitive, inputPast, inputPastParticiple, inputGeround, inputSpanish);
 });
+
+button.addEventListener('click', (event) => {
+  // alert("Funciona pa");
+  console.log(event)
+  
+})
 
 // for (let index = 0; index < 4; index++) {
 //   inputsAdding();
